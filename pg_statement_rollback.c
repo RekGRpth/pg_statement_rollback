499175c14ac49b7d61e002434f03820377d1f72f
Gilles Darold <gilles@darold.net>
2024-10-04 12:12:11 +0200

    Fix copyright year

--- pg_statement_rollback.c
+++ pg_statement_rollback.c
@@ -6,7 +6,7 @@
  *
  * Authors: Julien Rouhaud, Dave Sharpe, Gilles Darold 
  * Licence: PostgreSQL
- * Copyright (c) 2020-2023 LzLabs, GmbH
+ * Copyright (c) 2020-2024 LzLabs, GmbH
  *
  *-------------------------------------------------------------------------
  */
